f707814031ed1e1ca2fe99cab68044c7c51fcc5f
spchjken <spchjken@gmail.com>
2020-12-26 22:35:21 +0700

    Update __init__.py

--- pyfpgrowth/__init__.py
+++ pyfpgrowth/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-from .pyfpgrowth import generate_association_rules, find_frequent_patterns  # flake8: noqa
+from .pyfpgrowth Load_data, create_initialset, create_FPTree, Mine_Tree  # flake8: noqa
 
-__author__ = 'Evan Dempsey'
-__email__ = 'me@evandempsey.io'
-__version__ = '1.0'
