7b9e41e8443ac44a03bc0e800f6f37885c13bf93
spchjken <spchjken@gmail.com>
2020-12-26 22:45:55 +0700

    Update __init__.py

--- pyfpgrowth/__init__.py
+++ pyfpgrowth/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-from .pyfpgrowth Load_data, create_initialset, create_FPTree, Mine_Tree  # flake8: noqa
+from .pyfpgrowth import Load_data, create_initialset, create_FPTree, Mine_Tree  # flake8: noqa
 
